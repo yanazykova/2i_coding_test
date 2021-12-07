23c284d1ad75358d8c64657842325e1bd8f9fdd6
yanazykova <yvz1973@gmail.com>
2021-12-07 18:59:10 +0000

    deleted test files

--- multiples/multiples_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from multiples import *
-
-class TestMultiples(unittest.TestCase):
-    
-    def test_multiples(self):
-        self.assertEqual(0, multiples())
-        
--- multiples/run_tests.py
+++
@@ -1,5 +0,0 @@
-
-from multiples_test import *
-
-if __name__ == '__main__':
-    unittest.main()
